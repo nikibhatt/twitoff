046fb2c9ba9cc14705048e5346b364083314c904
nikibhatt <nkibhatt@users.noreply.github.com>
2019-12-16 16:25:16 -0800

    Twitoff app

---
+++ TWITOFF/app.py
@@ -0,0 +1,18 @@
+""" Code for our app"""
+
+from flask import Flask
+from .models import DB
+
+#Make the applicaiton
+def create_app():
+    app = Flask(__name__)
+
+    #add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    #have the database know about the applicaiton
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        return 'Welcome to Twitoff'
+    return app
